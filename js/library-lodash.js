a0da26cdbbdcc73e48a92951930b67954b3a2e1f
Anastasiya1301 <dudkoanastasiya1301@gmail.com>
2025-11-08 19:24:07 +0200

    js

--- js/library-lodash.js
+++ js/library-lodash.js
@@ -17,10 +17,48 @@
 
 
 //DEBOUNCE - гарантує, що функц.
-//  не буде вклакана знову, поки не пройде
+//  не буде виклакана знову, поки не пройде
 // певна к-сть часу без її виклику
 
 //режими дебоунсе
 //trailing edge(в кінці)
 //leading edge(на початку)
+
+
+// const coordsOutputEl = document.querySelector(".js-coords");
+// console.log(coordsOutputEl.textContent);
+
+// //THROTTLE
+// //_ - бібліотека лодеш
+// const throttleMouseMove = _.throttle(onCoordsOutputElMouseMove, 1000);
+// let mouseMoveCounter = 0;
+// // window.addEventListener("mousemove", onCoordsOutputElMouseMove);
+// window.addEventListener("mousemove", throttleMouseMove);
+// function onCoordsOutputElMouseMove(event) {
+//     mouseMoveCounter += 1;
+//     coordsOutputEl.textContent = `Кількість викликів функцій: ${mouseMoveCounter},
+//     x: ${event.clientX}, y: ${event.clientY}`;
+
+// }
+
+
+//DEBOUNCE
+
+// const inputElement = document.querySelector(".js-input");
+
+// const textElement = document.querySelector("js-output");
+
+
+// let inputCounter = 0;
+
+// inputElement.addEventListener("input", _.debounce(onInputElementChange), 1000);
+// // inputElement.addEventListener("input", onInputElementChange);
+// function onInputElementChange(event) {
+//     inputCounter += 1;
+//     textElement.textContent = `Кількість викликів функцій: ${inputCounter}
+//     Значення: ${event.target.value}`;
+// }
+
+
+
 
